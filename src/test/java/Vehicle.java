cd840813406d35ec78e2da9e990e251758bb73eb
Deepak2202 <deepakvjms@gmail.com>
2024-04-13 11:28:25 +0530

    first time push

---
+++ src/test/java/Vehicle.java
@@ -0,0 +1,19 @@
+
+public class Vehicle {
+	
+	private void car() {
+		System.out.println("four wheeler");
+		
+ }
+	
+	private void auto() {
+		System.out.println("three wheeler");
+		
+	}
+	
+	private void bike() {
+		System.out.println("two wheeler");
+
+	}
+
+}
